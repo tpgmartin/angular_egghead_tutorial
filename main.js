d08c5a96994e2f8dd0c939c4d51013f081eb8f10
Thomas Martin <tpgmartin@gmail.com>
2014-05-12 14:48:34 +0100

    Using filters rather than defining methods scope

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 var myApp = angular.module('myApp', []);
-myApp.factory('Data', function() {
+myApp.factory('Data', function() { // Name service 'Data'
     return {message: "Hello World!"}
+})
+
+myApp.filter('reverse', function (Data) {
+  return function (text) {
+    return text.split("").reverse().join("") + Data.message;
+  }
 })
 
 function FirstCtrl($scope, Data){
