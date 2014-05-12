b955db8d70ce8a5e3d9c41fa261f2a9bc93dd020
Thomas Martin <tpgmartin@gmail.com>
2014-05-12 14:29:10 +0100

    Completed tutorial: sharing data between controllers

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-function FirstCtrl($scope){
-  $scope.data = {message: "Hello"};
+var myApp = angular.module('myApp', []);
+myApp.factory('Data', function() {
+    return {message: "Hello World!"}
+})
+
+function FirstCtrl($scope, Data){
+  $scope.data = Data;
+}
+
+function SecondCtrl($scope, Data){
+  $scope.data = Data;
 }
