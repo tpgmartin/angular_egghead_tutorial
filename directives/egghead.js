c34db17fecb03e8ca8c268c580acf081d3137b4e
Thomas Martin <tpgmartin@gmail.com>
2014-05-13 14:30:06 +0100

    Writing first custom directive

---
+++ directives/egghead.js
@@ -0,0 +1,13 @@
+var egghead = angular.module("egghead", []);
+
+egghead.controller("AppCtrl", function () {
+    var app = this;
+
+    app.message = "Hello";
+})
+
+egghead.directive("myFirstDirective", function () {
+    return function (scope, element, attrs) {
+        element.text(scope.app.message + " " + attrs.message);
+    }
+})
